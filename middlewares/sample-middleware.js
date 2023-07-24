0e379a9872fb808635d7aefaec7397d14fda78f1
Xcoder <chindaebube03@gmail.comm>
2023-07-24 21:45:08 +0100

    created errorHandler controller

---
+++ middlewares/sample-middleware.js
@@ -0,0 +1,11 @@
+// this folder acts as your helper functions(login, validation) just like the utils folder 
+// but with slight difference
+
+export const sampleMiddleware = async(req,res) => {
+    try {
+        return res.json({ })
+    } catch (error) {
+        
+    }
+   
+}
